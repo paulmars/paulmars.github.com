887be346b71655610042483d79911c5247856b53
Paul McKellar <paul.mckellar@gmail.com>
2012-02-06 16:03:47 -0800

    move script

--- download_s3.rb
+++ download_s3.rb
@@ -1,6 +1,16 @@
 require 'rubygems'
 require 'yaml'
 
+
+names = Dir.glob("_photos/*")
+urls = names.map {|n|
+  hash = YAML.load(File.open(n).read)["filehash"]
+  "http://s3.amazonaws.com/juno/#{hash}_large.JPG"
+}.uniq
+Dir.chdir("images")
+urls.each {|u| `wget #{u}` }
+
+
 names = Dir.glob("_photos/*")
 tuples = names.map do |name|
   hash = YAML.load(File.open(name).read)
